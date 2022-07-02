a144ddd2178a5b87eb2ab7aaea464e88c874549b
MichaelSpencer11 <mspencer009@gmail.com>
2022-07-01 17:37:01 -0700

    wrote removeDuplicates

---
+++ src/components/UtilityMethods.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: UtilityMethods,
+        methods: {
+            removeDuplicates: function (array) {
+                var current = array[0]
+                for(var i = 0; i<array.length; i++){
+                    if (current === array[i+1]){
+                        array.pop(array[i+1])
+                        i--
+                    } else {
+                        current = array[i+1]
+                    }
+                }
+                return array
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
